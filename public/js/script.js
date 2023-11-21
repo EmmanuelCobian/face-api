1f1d88d8fb89dab6c7faed8b982a46ae2f593d1b
Winnie Zhu <winniezhu@berkeley.edu>
2023-11-21 15:24:04 -0800

    adding interval

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,18 @@
 const expression = document.getElementById('expression');
 const button = document.getElementById('button');
 const expressions = ["neutral", "happy", "sad", "angry", "fearful", "disgusted", "surprised"];
-
+let nIntervId;
 
 function getRandomExpression() {
     console.log("clicked");
     expression.innerHTML = expressions[Math.floor(Math.random() * expressions.length)];
 }
 
-button.addEventListener('click', getRandomExpression);
+function changeText() {
+    // check if an interval has already been set up
+    if (!nIntervId) {
+      nIntervId = setInterval(getRandomExpression, 1000);
+    }
+  }
+
+button.addEventListener('click', changeText);
